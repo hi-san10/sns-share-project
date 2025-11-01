f357227989197f152d2c54819eb12c7773c36070
hi-san <h.ahkh10@icloud.com>
2025-11-01 12:22:43 +0900

    ポストページ作成

---
+++ frontend/pages/posts/[id].vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="posts-container">
+        <SideNav />
+        <div>
+            <h1 class="container-title">コメント</h1>
+            <Message />
+            <h1>{{ $route.params.id }}</h1> <!-- id仮表示 -->
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.posts-container {
+    display: flex;
+    background-color: #15202B;
+}
+
+.container-title {
+    color: white;
+    margin: 50px 0 0 0 ;
+}
+
+h1 {
+    color: white;
+}
+</style>
